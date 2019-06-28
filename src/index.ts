48884b9babc27bfeaafbb0d59fcbb2b745eb2a75
Anthony A <anthony@hackages.io>
2019-06-28 14:14:26 +0200

    feat(useScrollblock): add useScrollblock Hook

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { usePrevious } from "./usePrevious";
-import { useInViewport } from "./useInViewport";
-import { useEvent } from "./useEvent";
-import { useClipboard } from "./useClipboard";
-import { useMouseOver } from "./useMouseOver";
+import { useScrollBlock } from './useScrollblock';
 
-export { usePrevious, useInViewport, useEvent, useMouseOver, useClipboard };
+  useScrollBlock,
